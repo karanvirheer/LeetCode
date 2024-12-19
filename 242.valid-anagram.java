7ae3fcc7b6e86ffb98654773afe127091c192fc9
karanvirheer <karanvirheer77@gmail.com>
2024-12-18 23:05:18 -0500

    Pretty simple but cool solution

---
+++ 242.valid-anagram.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=242 lang=java
+ *
+ * [242] Valid Anagram
+ */
+
+// @lc code=start
+
+import java.util.HashMap;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) - 1);
+        }
+
+        for (int v : map.values()) {
+            if (v != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+// @lc code=end
